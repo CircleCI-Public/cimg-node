3bd6c54139571f283b8eb0bdf7d7a6b28ea3fb0f
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-07-15 15:39:18 -0400

    Add v10.15.0. (#2)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 10.14/Dockerfile -t cimg/node:10.14.2  -t cimg/node:10.14 .
+docker build --file 10.15/Dockerfile -t cimg/node:10.15.0  -t cimg/node:10.15 .
