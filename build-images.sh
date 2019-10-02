58b639e7854aee33059ca252a4f8eeb98134a441
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-10-02 11:13:38 -0400

    Add Node.js v10.16.0.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 10.15/Dockerfile -t cimg/node:10.15.3  -t cimg/node:10.15 .
+docker build --file 10.16/Dockerfile -t cimg/node:10.16.0  -t cimg/node:10.16 .
