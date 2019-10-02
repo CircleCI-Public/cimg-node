92c8c59e361103061f72a3b3477715a48f9a2957
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-10-02 11:46:28 -0400

    Add Node.js v12.11.0.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 12.10/Dockerfile -t cimg/node:12.10.0  -t cimg/node:12.10 .
+docker build --file 12.11/Dockerfile -t cimg/node:12.11.0  -t cimg/node:12.11 .
