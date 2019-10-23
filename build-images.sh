e5439f54793bb4e9c073097434edff0cb55979ee
Ricardo N Feliciano <FelicianoTech@gmail.com>
2019-10-23 14:31:24 -0400

    Add v12.13.0. (#21)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 12.12/Dockerfile -t cimg/node:12.12.0  -t cimg/node:12.12 .
+docker build --file 12.13/Dockerfile -t cimg/node:12.13.0  -t cimg/node:12.13 .
