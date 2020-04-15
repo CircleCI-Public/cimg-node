b14b15b806db8560beb0fd8367b61c20328615a6
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-04-15 18:07:47 -0400

    Add v13.13.0. [release] (#64)

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 10.20/Dockerfile -t cimg/node:10.20.1  -t cimg/node:10.20 .
+docker build --file 13.13/Dockerfile -t cimg/node:13.13.0  -t cimg/node:13.13 .
