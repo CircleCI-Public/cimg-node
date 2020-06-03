4cff2de83dbd085490d57809c86fdc36bf25f530
James Billingham <git@jamesbillingham.com>
2020-06-03 09:36:23 +0100

    Publish v12.18.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 12.17/Dockerfile -t cimg/node:12.17.0  -t cimg/node:12.17 .
+docker build --file 12.18/Dockerfile -t cimg/node:12.18.0  -t cimg/node:12.18  -t cimg/node:lts .
