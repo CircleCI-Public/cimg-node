e886561fafaca7c8b1814c788065ee38cbb39506
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-08-11 19:44:00 -0400

    Add alias.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 14.8/Dockerfile -t cimg/node:14.8.0  -t cimg/node:14.8 .
+docker build --file 14.8/Dockerfile -t cimg/node:14.8.0  -t cimg/node:14.8 -t cimg/node:current .
