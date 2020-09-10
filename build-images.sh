69ec3a048c8abdcd0638ae1558f4c79d603ce200
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-09-10 11:14:20 -0400

    Publish v14.10.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 14.10/Dockerfile -t cimg/node:14.10.0  -t cimg/node:14.10  -t cimg/node:current .
+docker build --file 14.10/Dockerfile -t cimg/node:14.10.1  -t cimg/node:14.10  -t cimg/node:current .
