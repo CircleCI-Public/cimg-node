01b4c47b9f39c8a0b487aca6cdd2d5e7a7c23966
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-10-14 15:31:26 -0400

    Add browser variant.

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-docker build --file 14.13/Dockerfile -t cimg/node:14.13.1  -t cimg/node:14.13  -t cimg/node:current .
+docker build --file 10.22/browsers/Dockerfile -t cimg/node:10.22.1-browsers  -t cimg/node:10.22-browsers .
+docker build --file 12.18/browsers/Dockerfile -t cimg/node:12.18.4-browsers  -t cimg/node:12.18-browsers .
+docker build --file 12.19/browsers/Dockerfile -t cimg/node:12.19.0-browsers  -t cimg/node:12.19-browsers .
+docker build --file 14.12/browsers/Dockerfile -t cimg/node:14.12.0-browsers  -t cimg/node:14.12-browsers .
+docker build --file 14.13/browsers/Dockerfile -t cimg/node:14.13.1-browsers  -t cimg/node:14.13-browsers .
