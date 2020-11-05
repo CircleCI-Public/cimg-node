88b8544b686b40d87728c4f15a1091e5cfc054ba
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-05 12:22:44 -0500

    v12.19.0: Add xvfb to browsers variant. [release] (#111)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 10.23/Dockerfile -t cimg/node:10.23.0  -t cimg/node:10.23 .
-docker build --file 10.23/browsers/Dockerfile -t cimg/node:10.23.0-browsers  -t cimg/node:10.23-browsers .
-docker build --file 14.15/Dockerfile -t cimg/node:14.15.0  -t cimg/node:14.15  -t cimg/node:lts .
-docker build --file 14.15/browsers/Dockerfile -t cimg/node:14.15.0-browsers  -t cimg/node:14.15-browsers  -t cimg/node:lts-browsers .
+docker build --file 12.19/browsers/Dockerfile -t cimg/node:12.19.0-browsers  -t cimg/node:12.19-browsers .
