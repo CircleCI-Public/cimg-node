5a0f20c8f9a44c0c8133fd17189c7397e9a1ec19
Ricardo N Feliciano <FelicianoTech@gmail.com>
2020-11-13 19:05:58 -0500

    Respin browsers tags for xvfb, part 2. [release] (#115)

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 10.22/browsers/Dockerfile -t cimg/node:10.22.1-browsers  -t cimg/node:10.22-browsers .
-docker build --file 10.23/browsers/Dockerfile -t cimg/node:10.23.0-browsers  -t cimg/node:10.23-browsers .
-docker build --file 12.18/browsers/Dockerfile -t cimg/node:12.18.4-browsers  -t cimg/node:12.18-browsers .
-docker build --file 12.19/browsers/Dockerfile -t cimg/node:12.19.0-browsers  -t cimg/node:12.19-browsers .
-docker build --file 14.12/browsers/Dockerfile -t cimg/node:14.12.0-browsers  -t cimg/node:14.12-browsers .
-docker build --file 14.13/browsers/Dockerfile -t cimg/node:14.13.1-browsers  -t cimg/node:14.13-browsers .
-docker build --file 14.14/browsers/Dockerfile -t cimg/node:14.14.0-browsers  -t cimg/node:14.14-browsers .
-docker build --file 14.15/browsers/Dockerfile -t cimg/node:14.15.0-browsers  -t cimg/node:14.15-browsers .
-docker build --file 15.0/browsers/Dockerfile -t cimg/node:15.0.1-browsers  -t cimg/node:15.0-browsers .
+docker build --file 15.1/browsers/Dockerfile -t cimg/node:15.1.0-browsers  -t cimg/node:15.1-browsers .
+docker build --file 15.2/browsers/Dockerfile -t cimg/node:15.2.0-browsers  -t cimg/node:15.2-browsers  -t cimg/node:current-browsers .
