322b02f79d0bc5d7e51967aee0f17f39bb36cf2a
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-02-18 18:59:52 -0500

    Publish v15.9.0. [release] (#131)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 12.20/Dockerfile -t cimg/node:12.20.2  -t cimg/node:12.20 .
-docker build --file 12.20/browsers/Dockerfile -t cimg/node:12.20.2-browsers  -t cimg/node:12.20-browsers .
+docker build --file 15.9/Dockerfile -t cimg/node:15.9.0  -t cimg/node:15.9  -t cimg/node:current .
+docker build --file 15.9/browsers/Dockerfile -t cimg/node:15.9.0-browsers  -t cimg/node:15.9-browsers  -t cimg/node:current-browsers .
