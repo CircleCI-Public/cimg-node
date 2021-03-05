82fe8071dc0c020d9af1f70d7bbf9245cbfdda37
Ricardo N Feliciano <FelicianoTech@gmail.com>
2021-03-05 12:26:14 -0500

    Publish v15.11.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 10.24/Dockerfile -t cimg/node:10.24.0  -t cimg/node:10.24 .
-docker build --file 10.24/browsers/Dockerfile -t cimg/node:10.24.0-browsers  -t cimg/node:10.24-browsers .
-docker build --file 12.21/Dockerfile -t cimg/node:12.21.0  -t cimg/node:12.21 .
-docker build --file 12.21/browsers/Dockerfile -t cimg/node:12.21.0-browsers  -t cimg/node:12.21-browsers .
-docker build --file 14.16/Dockerfile -t cimg/node:14.16.0  -t cimg/node:14.16  -t cimg/node:lts .
-docker build --file 14.16/browsers/Dockerfile -t cimg/node:14.16.0-browsers  -t cimg/node:14.16-browsers  -t cimg/node:lts-browsers .
-docker build --file 15.10/Dockerfile -t cimg/node:15.10.0  -t cimg/node:15.10  -t cimg/node:current .
-docker build --file 15.10/browsers/Dockerfile -t cimg/node:15.10.0-browsers  -t cimg/node:15.10-browsers  -t cimg/node:current-browsers .
+docker build --file 15.11/Dockerfile -t cimg/node:15.11.0  -t cimg/node:15.11  -t cimg/node:current .
+docker build --file 15.11/browsers/Dockerfile -t cimg/node:15.11.0-browsers  -t cimg/node:15.11-browsers  -t cimg/node:current-browsers .
