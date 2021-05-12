e9fc96ab7ac1af3c64ec0b803ec82be2393234cd
Adam Hartley <git@ahartley.com>
2021-05-12 11:52:12 +0100

    Publish v14.17.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 16.1/Dockerfile -t cimg/node:16.1.0  -t cimg/node:16.1  -t cimg/node:current .
-docker build --file 16.1/browsers/Dockerfile -t cimg/node:16.1.0-browsers  -t cimg/node:16.1-browsers  -t cimg/node:current-browsers .
+docker build --file 14.17/Dockerfile -t cimg/node:14.17.0  -t cimg/node:14.17  -t cimg/node:lts .
+docker build --file 14.17/browsers/Dockerfile -t cimg/node:14.17.0-browsers  -t cimg/node:14.17-browsers  -t cimg/node:lts-browsers .
