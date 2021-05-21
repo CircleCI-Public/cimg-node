4a52aa97d74a7f969a11c2a5271fb03e5b320175
Ben Limmer <ben@benlimmer.com>
2021-05-20 18:14:14 -0600

    Publish v16.2.0. [release] (#148)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 14.17/Dockerfile -t cimg/node:14.17.0  -t cimg/node:14.17  -t cimg/node:lts .
-docker build --file 14.17/browsers/Dockerfile -t cimg/node:14.17.0-browsers  -t cimg/node:14.17-browsers  -t cimg/node:lts-browsers .
+docker build --file 16.2/Dockerfile -t cimg/node:16.2.0  -t cimg/node:16.2 .
+docker build --file 16.2/browsers/Dockerfile -t cimg/node:16.2.0-browsers  -t cimg/node:16.2-browsers .
