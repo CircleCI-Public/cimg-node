72d37fc35c4dfff57a451dd04ed490e1008f7c1a
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2021-08-03 21:57:47 -0700

    Publish v16.6.1. [release] (#158)
    Co-authored-by: Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>

--- build-images.sh
+++ build-images.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 12.22/Dockerfile -t cimg/node:12.22.4  -t cimg/node:12.22 .
-docker build --file 12.22/browsers/Dockerfile -t cimg/node:12.22.4-browsers  -t cimg/node:12.22-browsers .
-docker build --file 14.17/Dockerfile -t cimg/node:14.17.4  -t cimg/node:14.17  -t cimg/node:lts .
-docker build --file 14.17/browsers/Dockerfile -t cimg/node:14.17.4-browsers  -t cimg/node:14.17-browsers  -t cimg/node:lts-browsers .
-docker build --file 16.6/Dockerfile -t cimg/node:16.6.0  -t cimg/node:16.6  -t cimg/node:current .
-docker build --file 16.6/browsers/Dockerfile -t cimg/node:16.6.0-browsers  -t cimg/node:16.6-browsers  -t cimg/node:current-browsers .
+docker build --file 16.6/Dockerfile -t cimg/node:16.6.1  -t cimg/node:16.6  -t cimg/node:current .
+docker build --file 16.6/browsers/Dockerfile -t cimg/node:16.6.1-browsers  -t cimg/node:16.6-browsers  -t cimg/node:current-browsers .
