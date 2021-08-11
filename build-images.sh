4bd7a71a8bd78acf766ec3cda9d0071d1a0d8ee9
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2021-08-11 14:59:24 -0700

    Publish v12.22.5, v14.17.5, and v16.6.2. [release] (#160)
    Co-authored-by: Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 16.6/Dockerfile -t cimg/node:16.6.1  -t cimg/node:16.6  -t cimg/node:current .
-docker build --file 16.6/browsers/Dockerfile -t cimg/node:16.6.1-browsers  -t cimg/node:16.6-browsers  -t cimg/node:current-browsers .
+docker build --file 12.22/Dockerfile -t cimg/node:12.22.5  -t cimg/node:12.22 .
+docker build --file 12.22/browsers/Dockerfile -t cimg/node:12.22.5-browsers  -t cimg/node:12.22-browsers .
+docker build --file 14.17/Dockerfile -t cimg/node:14.17.5  -t cimg/node:14.17  -t cimg/node:lts .
+docker build --file 14.17/browsers/Dockerfile -t cimg/node:14.17.5-browsers  -t cimg/node:14.17-browsers  -t cimg/node:lts-browsers .
+docker build --file 16.6/Dockerfile -t cimg/node:16.6.2  -t cimg/node:16.6  -t cimg/node:current .
+docker build --file 16.6/browsers/Dockerfile -t cimg/node:16.6.2-browsers  -t cimg/node:16.6-browsers  -t cimg/node:current-browsers .
