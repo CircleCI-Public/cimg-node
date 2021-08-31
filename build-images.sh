3da5201fb95460c1de29c4c00c6caf6b453561f5
Michael Rothstein <michaelrothstein@Michaels-MacBook-Pro.local>
2021-08-31 12:06:27 -0700

    Publish v12.22.6 and v14.7.6. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build --file 14.17/Dockerfile -t cimg/node:14.17.5  -t cimg/node:14.17  -t cimg/node:lts .
-docker build --file 14.17/browsers/Dockerfile -t cimg/node:14.17.5-browsers  -t cimg/node:14.17-browsers  -t cimg/node:lts-browsers .
-docker build --file 16.8/Dockerfile -t cimg/node:16.8.0  -t cimg/node:16.8  -t cimg/node:current .
-docker build --file 16.8/browsers/Dockerfile -t cimg/node:16.8.0-browsers  -t cimg/node:16.8-browsers  -t cimg/node:current-browsers .
+docker build --file 12.22/Dockerfile -t cimg/node:12.22.6  -t cimg/node:12.22 .
+docker build --file 12.22/browsers/Dockerfile -t cimg/node:12.22.6-browsers  -t cimg/node:12.22-browsers .
+docker build --file 14.7/Dockerfile -t cimg/node:14.7.6  -t cimg/node:14.7  -t cimg/node:lts .
+docker build --file 14.7/browsers/Dockerfile -t cimg/node:14.7.6-browsers  -t cimg/node:14.7-browsers  -t cimg/node:lts-browsers .
