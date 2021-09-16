d24472e6d06256005cf073197057b7230dc7d1f9
Dragos Campean <campean_dragos@ymail.com>
2021-09-16 20:25:15 +0300

    Add docker image for node 12.15.0-browsers (#167) [release]
    * Add docker image for node 12.15.0-browsers
    
    * Additional fixs for publishing
    
    Co-authored-by: Ricardo N Feliciano <FelicianoTech@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 12.22/Dockerfile -t cimg/node:12.22.6  -t cimg/node:12.22 .
-docker build --file 12.22/browsers/Dockerfile -t cimg/node:12.22.6-browsers  -t cimg/node:12.22-browsers .
-docker build --file 14.17/Dockerfile -t cimg/node:14.17.6  -t cimg/node:14.17  -t cimg/node:lts .
-docker build --file 14.17/browsers/Dockerfile -t cimg/node:14.17.6-browsers  -t cimg/node:14.17-browsers  -t cimg/node:lts-browsers .
+docker build --file 12.15/browsers/Dockerfile -t cimg/node:12.15.0-browsers  -t cimg/node:12.15-browsers .
