920aceab6a4bff4930e6782ecfc8e799a43174cd
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2021-09-22 16:13:39 -0700

    Publish v16.10.0. [release] (#171)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 16.9/Dockerfile -t cimg/node:16.9.0  -t cimg/node:16.9  -t cimg/node:current .
-docker build --file 16.9/browsers/Dockerfile -t cimg/node:16.9.0-browsers  -t cimg/node:16.9-browsers  -t cimg/node:current-browsers .
+docker build --file 16.10/Dockerfile -t cimg/node:16.10.0  -t cimg/node:16.10  -t cimg/node:current .
+docker build --file 16.10/browsers/Dockerfile -t cimg/node:16.10.0-browsers  -t cimg/node:16.10-browsers  -t cimg/node:current-browsers .
