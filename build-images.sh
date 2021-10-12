2cfb653e30431463bd0d1bdbb7370067acdf9c2f
mrothstein74 <michael.rothstein@circleci.com>
2021-10-12 09:45:00 -0700

    Publish v12.22.7, v14.18.1, and v16.11.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-docker build --file 16.11/Dockerfile -t cimg/node:16.11.0  -t cimg/node:16.11  -t cimg/node:current .
-docker build --file 16.11/browsers/Dockerfile -t cimg/node:16.11.0-browsers  -t cimg/node:16.11-browsers  -t cimg/node:current-browsers .
+docker build --file 12.22/Dockerfile -t cimg/node:12.22.7  -t cimg/node:12.22 .
+docker build --file 12.22/browsers/Dockerfile -t cimg/node:12.22.7-browsers  -t cimg/node:12.22-browsers .
+docker build --file 14.18/Dockerfile -t cimg/node:14.18.1  -t cimg/node:14.18  -t cimg/node:lts .
+docker build --file 14.18/browsers/Dockerfile -t cimg/node:14.18.1-browsers  -t cimg/node:14.18-browsers  -t cimg/node:lts-browsers .
+docker build --file 16.11/Dockerfile -t cimg/node:16.11.1  -t cimg/node:16.11  -t cimg/node:current .
+docker build --file 16.11/browsers/Dockerfile -t cimg/node:16.11.1-browsers  -t cimg/node:16.11-browsers  -t cimg/node:current-browsers .
