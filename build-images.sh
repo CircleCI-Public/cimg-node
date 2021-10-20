b31b39c039c91dd367fc201bcb3986c38a2a17ae
mrothstein74 <michael.rothstein@circleci.com>
2021-10-20 14:33:37 -0700

    Publish v17.0.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file /Dockerfile -t cimg/node:cd  -t cimg/node: .
-docker build --file /browsers/Dockerfile -t cimg/node:cd-browsers  -t cimg/node:-browsers .
-docker build --file /Dockerfile -t cimg/node:../..  -t cimg/node: .
-docker build --file /browsers/Dockerfile -t cimg/node:../..-browsers  -t cimg/node:-browsers .
+docker build --file 17.0/Dockerfile -t cimg/node:17.0.1  -t cimg/node:17.0  -t cimg/node:current .
+docker build --file 17.0/browsers/Dockerfile -t cimg/node:17.0.1-browsers  -t cimg/node:17.0-browsers  -t cimg/node:current-browsers .
