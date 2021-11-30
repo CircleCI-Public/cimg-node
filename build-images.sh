ce45b08bf2cca4fbcfd4fc4f804ef317e1736077
mrothstein74 <80537164+mrothstein74@users.noreply.github.com>
2021-11-30 12:29:06 -0800

    Publish v17.2.0. [release] (#188)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 14.18/Dockerfile -t cimg/node:14.18.2  -t cimg/node:14.18 .
-docker build --file 14.18/browsers/Dockerfile -t cimg/node:14.18.2-browsers  -t cimg/node:14.18-browsers .
+docker build --file 17.2/Dockerfile -t cimg/node:17.2.0  -t cimg/node:17.2  -t cimg/node:current .
+docker build --file 17.2/browsers/Dockerfile -t cimg/node:17.2.0-browsers  -t cimg/node:17.2-browsers  -t cimg/node:current-browsers .
