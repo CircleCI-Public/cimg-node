26a0222221c922ce780bad53d94325f38f5825ab
Adam Hartley <BytesGuy@users.noreply.github.com>
2021-12-01 17:16:15 +0000

    Publish v16.13.1. [release] (#189)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 17.2/Dockerfile -t cimg/node:17.2.0  -t cimg/node:17.2  -t cimg/node:current .
-docker build --file 17.2/browsers/Dockerfile -t cimg/node:17.2.0-browsers  -t cimg/node:17.2-browsers  -t cimg/node:current-browsers .
+docker build --file 16.13/Dockerfile -t cimg/node:16.13.1  -t cimg/node:16.13  -t cimg/node:lts .
+docker build --file 16.13/browsers/Dockerfile -t cimg/node:16.13.1-browsers  -t cimg/node:16.13-browsers  -t cimg/node:lts-browsers .
