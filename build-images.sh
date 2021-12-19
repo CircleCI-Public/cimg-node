354ed519ea579e440d9d0656e5a63bd3a1f99af7
JalexChen <jeff.chen@circleci.com>
2021-12-18 18:24:20 -0800

    Publish v12.22.8. [release] (#190)
    * Publish v12.22.8. [release]
    
    * Update ALIASES
    
    Co-authored-by: Ricardo N Feliciano <FelicianoTech@gmail.com>

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 16.13/Dockerfile -t cimg/node:16.13.1  -t cimg/node:16.13  -t cimg/node:lts .
-docker build --file 16.13/browsers/Dockerfile -t cimg/node:16.13.1-browsers  -t cimg/node:16.13-browsers  -t cimg/node:lts-browsers .
+docker build --file 12.22/Dockerfile -t cimg/node:12.22.8  -t cimg/node:12.22  -t cimg/node:lts .
+docker build --file 12.22/browsers/Dockerfile -t cimg/node:12.22.8-browsers  -t cimg/node:12.22-browsers  -t cimg/node:lts-browsers .
