bb2994da92e0a2abc1308935598658e1f4dc3f0d
JalexChen <jeff.chen@circleci.com>
2022-01-10 13:16:31 -0800

    Check Versions Script (#193)
    * Check Alias LTS and Current Versions
    
    * Fix where the script runs
    
    Co-authored-by: Ricardo N Feliciano <FelicianoTech@gmail.com>

---
+++ check-versions.sh
@@ -0,0 +1,33 @@
+echo "Checking Versioning"
+
+source ./ALIASES
+currentLTS=${lts}
+currentVersion=${current}
+
+git restore ALIASES --source=master
+prevLTS=$(grep 'lts' ALIASES | cut -d "=" -f2)
+prevVersion=$(grep 'current' ALIASES | cut -d "=" -f2)
+
+if [[ $prevVersion != $currentVersion ]]; then
+    if [ "$(printf '%s\n' "$currentVersion" "$prevVersion" | sort -V | head -n1)" = "$currentVersion" ]; then
+        echo "Please use an updated current version"
+        exit 1
+    else
+        grep -v "current" ./ALIASES > ./TEMP && mv ./TEMP ./ALIASES
+        echo "current=${currentVersion}" >> ALIASES
+        echo "Current version check passed"
+    fi
+fi
+
+if [[ $prevLTS != $currentLTS ]]; then
+    if [ "$(printf '%s\n' "$currentLTS" "$prevLTS" | sort -V | head -n1)" = "$currentLTS" ]; then
+        echo "Please use an updated LTS version" 
+        exit 1
+    else
+        grep -v "lts" ./ALIASES > ./TEMP && mv ./TEMP ./ALIASES
+        echo "lts=${currentLTS}" >> ALIASES
+        echo "LTS version check passed"
+    fi
+fi
+
+echo "LTS and Current version checks completed"
