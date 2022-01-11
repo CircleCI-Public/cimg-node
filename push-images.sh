3c09de237dcce789d8396bae591a03e86e2b61c3
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2022-01-11 11:20:19 -0500

    Bump security releases

--- push-images.sh
+++ push-images.sh
@@ -1,1 +1,23 @@
 #!/usr/bin/env bash
+
+docker push cimg/node:12.22.9
+docker push cimg/node:12.22
+docker push cimg/node:12.22.9-browsers
+docker push cimg/node:12.22-browsers
+
+docker push cimg/node:14.18.3
+docker push cimg/node:14.18
+docker push cimg/node:14.18.3-browsers
+docker push cimg/node:14.18-browsers
+
+docker push cimg/node:16.13.2
+docker push cimg/node:16.13
+docker push cimg/node:lts
+docker push cimg/node:16.13.2-browsers
+docker push cimg/node:16.13-browsers
+docker push cimg/node:lts-browsers
+
+docker push cimg/node:17.3.1
+docker push cimg/node:17.3
+docker push cimg/node:17.3.1-browsers
+docker push cimg/node:17.3-browsers
