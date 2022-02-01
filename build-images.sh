e7c2099413b48facbf6f07d48d510dbf1e0ab479
Jalexchen <jeff.chen@circleci.com>
2022-02-01 09:19:38 -0800

    Publish v14.19.0. [release] (#206)

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 17.4/Dockerfile -t cimg/node:17.4.0  -t cimg/node:17.4  -t cimg/node:current .
-docker build --file 17.4/browsers/Dockerfile -t cimg/node:17.4.0-browsers  -t cimg/node:17.4-browsers  -t cimg/node:current-browsers .
+docker build --file 14.19/Dockerfile -t cimg/node:14.19.0  -t cimg/node:14.19 .
+docker build --file 14.19/browsers/Dockerfile -t cimg/node:14.19.0-browsers  -t cimg/node:14.19-browsers .
