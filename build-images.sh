6078ab65714d2332e1879f40c987ec0116b9037d
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-02-01 17:44:07 -0500

    Publish v12.22.10. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 14.19/Dockerfile -t cimg/node:14.19.0  -t cimg/node:14.19 .
-docker build --file 14.19/browsers/Dockerfile -t cimg/node:14.19.0-browsers  -t cimg/node:14.19-browsers .
+docker build --file 12.22/Dockerfile -t cimg/node:12.22.10  -t cimg/node:12.22 .
+docker build --file 12.22/browsers/Dockerfile -t cimg/node:12.22.10-browsers  -t cimg/node:12.22-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/node:14.19.0
-docker push cimg/node:14.19
-docker push cimg/node:14.19.0-browsers
-docker push cimg/node:14.19-browsers
+docker push cimg/node:12.22.10
+docker push cimg/node:12.22
+docker push cimg/node:12.22.10-browsers
+docker push cimg/node:12.22-browsers
