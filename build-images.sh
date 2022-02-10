536c4fa29629fccd4669ae3d26427840bc2dacd8
Jeff Chen <jeff.chen@circleci.com>
2022-02-10 11:29:42 -0800

    Publish v17.5.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 16.14/Dockerfile -t cimg/node:16.14.0  -t cimg/node:16.14  -t cimg/node:lts .
-docker build --file 16.14/browsers/Dockerfile -t cimg/node:16.14.0-browsers  -t cimg/node:16.14-browsers  -t cimg/node:lts-browsers .
+docker build --file 17.5/Dockerfile -t cimg/node:17.5.0  -t cimg/node:17.5  -t cimg/node:current .
+docker build --file 17.5/browsers/Dockerfile -t cimg/node:17.5.0-browsers  -t cimg/node:17.5-browsers  -t cimg/node:current-browsers .
