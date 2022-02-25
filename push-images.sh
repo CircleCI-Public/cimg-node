b3d7f7577c27101dd2dde4a96c14284b63c01fd8
Jalexchen <jeff.chen@circleci.com>
2022-02-25 10:33:53 -0800

    Publish v17.6.0. [release] (#213)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/node:17.5.0
-docker push cimg/node:17.5
+docker push cimg/node:17.6.0
+docker push cimg/node:17.6
 docker push cimg/node:current
-docker push cimg/node:17.5.0-browsers
-docker push cimg/node:17.5-browsers
+docker push cimg/node:17.6.0-browsers
+docker push cimg/node:17.6-browsers
 docker push cimg/node:current-browsers
