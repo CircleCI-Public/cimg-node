339e6e0dab7374d1b9aa0bc244d1c462f85242a6
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-03-10 22:36:47 +0000

    Publish v17.7.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 17.6/Dockerfile -t cimg/node:17.6.0  -t cimg/node:17.6  -t cimg/node:current .
-docker build --file 17.6/browsers/Dockerfile -t cimg/node:17.6.0-browsers  -t cimg/node:17.6-browsers  -t cimg/node:current-browsers .
+docker build --file 17.7/Dockerfile -t cimg/node:17.7.1  -t cimg/node:17.7  -t cimg/node:current .
+docker build --file 17.7/browsers/Dockerfile -t cimg/node:17.7.1-browsers  -t cimg/node:17.7-browsers  -t cimg/node:current-browsers .
