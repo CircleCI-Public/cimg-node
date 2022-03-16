1770ea4d96f814121379ed323be128d357047c0e
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-03-16 10:55:27 +0000

    Publish v16.14.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/node:17.7.1
-docker push cimg/node:17.7
-docker push cimg/node:current
-docker push cimg/node:17.7.1-browsers
-docker push cimg/node:17.7-browsers
-docker push cimg/node:current-browsers
+docker push cimg/node:16.14.1
+docker push cimg/node:16.14
+docker push cimg/node:lts
+docker push cimg/node:16.14.1-browsers
+docker push cimg/node:16.14-browsers
+docker push cimg/node:lts-browsers
