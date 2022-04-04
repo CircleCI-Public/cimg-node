480f3d9a961a6bee79dc12bbfdebd9d41927694d
Jeff Chen <jeff.chen@circleci.com>
2022-04-04 15:15:19 -0700

    testing build output

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
+set +x 
+
 docker build --file 17.8/Dockerfile -t cimg/node:17.8.0  -t cimg/node:17.8  -t cimg/node:current .
 docker build --file 17.8/browsers/Dockerfile -t cimg/node:17.8.0-browsers  -t cimg/node:17.8-browsers  -t cimg/node:current-browsers .
+
+echo "did build work"
