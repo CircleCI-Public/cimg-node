71ed7c91dff503d9e3462e05e4b2f66ae7cad962
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-04-05 15:20:35 +0100

    Publish v12.22.12. [release] (#222)

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/node:17.8.0
-docker push cimg/node:17.8
-docker push cimg/node:current
-docker push cimg/node:17.8.0-browsers
-docker push cimg/node:17.8-browsers
-docker push cimg/node:current-browsers
+docker push cimg/node:12.22.12
+docker push cimg/node:12.22
+docker push cimg/node:12.22.12-browsers
+docker push cimg/node:12.22-browsers
