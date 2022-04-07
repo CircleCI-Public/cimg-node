c7e34b6b626ddbf4d25449465fa45d30ef1ad44f
Jeff Chen <jeff.chen@circleci.com>
2022-04-07 12:41:53 -0700

    test orb with script

--- build-images.sh
+++ build-images.sh
@@ -3,5 +3,3 @@
 
 docker build --file 17.8/Dockerfile -t cimg/node:17.8.0  -t cimg/node:17.8  -t cimg/node:current .
 docker build --file 17.8/browsers/Dockerfile -t cimg/node:17.8.0-browsers  -t cimg/node:17.8-browsers  -t cimg/node:current-browsers .
-
-echo "test"
