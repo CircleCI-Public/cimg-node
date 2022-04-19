1b86b868fb54406b786d12d0773d344b2a03c7ad
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-04-19 17:18:58 -0400

    Publish v18.0.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 17.9/Dockerfile -t cimg/node:17.9.0  -t cimg/node:17.9  -t cimg/node:current .
-docker build --file 17.9/browsers/Dockerfile -t cimg/node:17.9.0-browsers  -t cimg/node:17.9-browsers  -t cimg/node:current-browsers .
+docker build --file 18.0/Dockerfile -t cimg/node:18.0.0  -t cimg/node:18.0  -t cimg/node:current .
+docker build --file 18.0/browsers/Dockerfile -t cimg/node:18.0.0-browsers  -t cimg/node:18.0-browsers  -t cimg/node:current-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/node:17.9.0
-docker push cimg/node:17.9
+docker push cimg/node:18.0.0
+docker push cimg/node:18.0
 docker push cimg/node:current
-docker push cimg/node:17.9.0-browsers
-docker push cimg/node:17.9-browsers
+docker push cimg/node:18.0.0-browsers
+docker push cimg/node:18.0-browsers
 docker push cimg/node:current-browsers
