07c05173bcfc193974343315f07361fb79fa9a25
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-05-03 10:38:35 -0400

    Publish v12.13.1. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/node:16.15.0
-docker push cimg/node:16.15
-docker push cimg/node:lts
-docker push cimg/node:16.15.0-browsers
-docker push cimg/node:16.15-browsers
-docker push cimg/node:lts-browsers
+docker push cimg/node:12.13.1
+docker push cimg/node:12.13
+docker push cimg/node:12.13.1-browsers
+docker push cimg/node:12.13-browsers
