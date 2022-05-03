3ecf5d15fa30d0c2c2aca9e69d8fc0ec0db88eb8
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-05-03 10:53:50 -0400

    Publish v12.13.1. [release] (#233)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 16.15/Dockerfile -t cimg/node:16.15.0  -t cimg/node:16.15  -t cimg/node:lts .
-docker build --file 16.15/browsers/Dockerfile -t cimg/node:16.15.0-browsers  -t cimg/node:16.15-browsers  -t cimg/node:lts-browsers .
+docker build --file 12.13/Dockerfile -t cimg/node:12.13.1  -t cimg/node:12.13 .
+docker build --file 12.13/browsers/Dockerfile -t cimg/node:12.13.1-browsers  -t cimg/node:12.13-browsers .
