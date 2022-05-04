e7b22b5e02c70b9ac6d82c5180b03ccdd4814536
Jeff Chen <jeff.chen@circleci.com>
2022-05-04 13:13:18 -0700

    Publish v14.19.2. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 12.13/Dockerfile -t cimg/node:12.13.1  -t cimg/node:12.13 .
-docker build --file 12.13/browsers/Dockerfile -t cimg/node:12.13.1-browsers  -t cimg/node:12.13-browsers .
+docker build --file 14.19/Dockerfile -t cimg/node:14.19.2  -t cimg/node:14.19 .
+docker build --file 14.19/browsers/Dockerfile -t cimg/node:14.19.2-browsers  -t cimg/node:14.19-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker push cimg/node:12.13.1
-docker push cimg/node:12.13
-docker push cimg/node:12.13.1-browsers
-docker push cimg/node:12.13-browsers
+docker push cimg/node:14.19.2
+docker push cimg/node:14.19
+docker push cimg/node:14.19.2-browsers
+docker push cimg/node:14.19-browsers
