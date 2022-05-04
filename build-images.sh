e847c7b7e0b36e1963e5d24701789067c15d5c8b
Jeff Chen <jeff.chen@circleci.com>
2022-05-04 09:44:27 -0700

    Publish v18.1.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 12.13/Dockerfile -t cimg/node:12.13.1  -t cimg/node:12.13 .
-docker build --file 12.13/browsers/Dockerfile -t cimg/node:12.13.1-browsers  -t cimg/node:12.13-browsers .
+docker build --file 18.1/Dockerfile -t cimg/node:18.1.0  -t cimg/node:18.1  -t cimg/node:current .
+docker build --file 18.1/browsers/Dockerfile -t cimg/node:18.1.0-browsers  -t cimg/node:18.1-browsers  -t cimg/node:current-browsers .
