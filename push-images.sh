65d606aa98ab937c7044815b5e1d567f0fb94696
Jeff Chen <jeff.chen@circleci.com>
2022-05-10 12:43:16 -0700

    Pub: v12.13.1, v14.19.2, v16.15.0, and more. [release]

--- push-images.sh
+++ push-images.sh
@@ -13,8 +13,10 @@ docker push cimg/node:14.19-browsers
 
 docker push cimg/node:16.15.0
 docker push cimg/node:16.15
+docker push cimg/node:lts
 docker push cimg/node:16.15.0-browsers
 docker push cimg/node:16.15-browsers
+docker push cimg/node:lts-browsers
 
 docker push cimg/node:18.1.0
 docker push cimg/node:18.1
