f5961e5cf5adc4232f5c49921beaaeb5bbb4a7ce
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-05-11 12:25:10 -0400

    Respin recent image tags for nvm fix [release]

--- push-images.sh
+++ push-images.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
+docker push cimg/node:12.13.1
+docker push cimg/node:12.13
+docker push cimg/node:12.13.1-browsers
+docker push cimg/node:12.13-browsers
+
+docker push cimg/node:14.19.2
+docker push cimg/node:14.19
+docker push cimg/node:14.19.2-browsers
+docker push cimg/node:14.19-browsers
+
 docker push cimg/node:16.15.0
 docker push cimg/node:16.15
 docker push cimg/node:lts
