22b58ad7ef3c934a70a99189439ec1f7731c5770
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-05-11 16:17:53 -0400

    Respin recent image tags for nvm fix [release] (#243)

--- build-images.sh
+++ build-images.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
+docker build --file 12.13/Dockerfile -t cimg/node:12.13.1  -t cimg/node:12.13 .
+docker build --file 12.13/browsers/Dockerfile -t cimg/node:12.13.1-browsers  -t cimg/node:12.13-browsers .
+docker build --file 14.19/Dockerfile -t cimg/node:14.19.2  -t cimg/node:14.19 .
+docker build --file 14.19/browsers/Dockerfile -t cimg/node:14.19.2-browsers  -t cimg/node:14.19-browsers .
 docker build --file 16.15/Dockerfile -t cimg/node:16.15.0  -t cimg/node:16.15  -t cimg/node:lts .
 docker build --file 16.15/browsers/Dockerfile -t cimg/node:16.15.0-browsers  -t cimg/node:16.15-browsers  -t cimg/node:lts-browsers .
 docker build --file 18.1/Dockerfile -t cimg/node:18.1.0  -t cimg/node:18.1  -t cimg/node:current .
