4c9c492a5c1d2d196d0e2f683c29e6afcc01c733
Jalexchen <jeff.chen@circleci.com>
2022-05-12 08:58:16 -0700

    spin lts and current [release] (#249)
    * spin lts and current
    
    * adding [release] to commit

--- push-images.sh
+++ push-images.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:12.13
-docker push cimg/node:12.13.1
-docker push cimg/node:12.13-browsers
-docker push cimg/node:12.13.1-browsers
-docker push cimg/node:14.19
-docker push cimg/node:14.19.2
-docker push cimg/node:14.19-browsers
-docker push cimg/node:14.19.2-browsers
-docker push cimg/node:16.15
+
 docker push cimg/node:16.15.0
-docker push cimg/node:16.15-browsers
+docker push cimg/node:16.15
+docker push cimg/node:lts
 docker push cimg/node:16.15.0-browsers
-docker push cimg/node:18.1
+docker push cimg/node:16.15-browsers
+docker push cimg/node:lts-browsers
+
 docker push cimg/node:18.1.0
-docker push cimg/node:18.1-browsers
+docker push cimg/node:18.1
+docker push cimg/node:current
 docker push cimg/node:18.1.0-browsers
+docker push cimg/node:18.1-browsers
+docker push cimg/node:current-browsers 
