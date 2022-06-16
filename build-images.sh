066c9726ec83cc913e860fca07927ee64c2b89be
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-06-16 16:28:09 +0100

    Publish v18.4.0. [release] (#260)

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 18.3/Dockerfile -t cimg/node:18.3.0 -t cimg/node:18.3 .
-docker build --file 18.3/browsers/Dockerfile -t cimg/node:18.3.0-browsers -t cimg/node:18.3-browsers .
-docker build --file 16.15/Dockerfile -t cimg/node:16.15.1 -t cimg/node:16.15 .
-docker build --file 16.15/browsers/Dockerfile -t cimg/node:16.15.1-browsers -t cimg/node:16.15-browsers .
-docker build --file 17.9/Dockerfile -t cimg/node:17.9.1 -t cimg/node:17.9 .
-docker build --file 17.9/browsers/Dockerfile -t cimg/node:17.9.1-browsers -t cimg/node:17.9-browsers .
+docker build --file 18.4/Dockerfile -t cimg/node:18.4.0 -t cimg/node:18.4 .
+docker build --file 18.4/browsers/Dockerfile -t cimg/node:18.4.0-browsers -t cimg/node:18.4-browsers .
