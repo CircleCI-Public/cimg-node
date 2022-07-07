0d3085881276939fb8f8a0933f8b644aa42abb12
Jeff Chen <jeff.chen@circleci.com>
2022-07-06 17:16:50 -0700

    add current tag back to file

--- push-images.sh
+++ push-images.sh
@@ -2,5 +2,9 @@
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/node:18.4
 docker push cimg/node:18.4.0
+docker tag cimg/node:18.4.0 cimg/node:current
+docker push cimg/node:current
 docker push cimg/node:18.4-browsers
 docker push cimg/node:18.4.0-browsers
+docker tag cimg/node:18.4.0-browsers cimg/node:current-browsers
+docker push cimg/node:current-browsers
