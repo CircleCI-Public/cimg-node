2d80121cf064e1c8b5b7c483d0bcb219b4697363
Tom Welsh <tom@gridsight.ai>
2022-07-12 04:16:45 +1000

    Publish v16.16.0. [release] (#264)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 18.4/Dockerfile -t cimg/node:18.4.0 -t cimg/node:18.4 .
-docker build --file 18.4/browsers/Dockerfile -t cimg/node:18.4.0-browsers -t cimg/node:18.4-browsers .
+docker build --file 16.16/Dockerfile -t cimg/node:16.16.0 -t cimg/node:16.16 .
+docker build --file 16.16/browsers/Dockerfile -t cimg/node:16.16.0-browsers -t cimg/node:16.16-browsers .
