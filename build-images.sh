195a512d16439f843599dd96f0f56997a6389754
Stuart Chinery <stuart.chinery@boxt.co.uk>
2022-07-22 11:14:54 +0100

    Publish v14.20.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 16.16/Dockerfile -t cimg/node:16.16.0 -t cimg/node:16.16 .
-docker build --file 16.16/browsers/Dockerfile -t cimg/node:16.16.0-browsers -t cimg/node:16.16-browsers .
+docker build --file 14.20/Dockerfile -t cimg/node:14.20.0 -t cimg/node:14.20 .
+docker build --file 14.20/browsers/Dockerfile -t cimg/node:14.20.0-browsers -t cimg/node:14.20-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:16.16
-docker push cimg/node:16.16.0
-docker tag cimg/node:16.16.0 cimg/node:lts
-docker push cimg/node:lts
-docker push cimg/node:16.16-browsers
-docker push cimg/node:16.16.0-browsers
-docker tag cimg/node:16.16.0-browsers cimg/node:lts-browsers
-docker push cimg/node:lts-browsers
+docker push cimg/node:14.20
+docker push cimg/node:14.20.0
+docker push cimg/node:14.20-browsers
+docker push cimg/node:14.20.0-browsers
