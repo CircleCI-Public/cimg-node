6c4dd23c2ac191c107e1b6ac1438b2b80284a8f4
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-08-26 10:54:56 -0400

    Publish v18.8.0. [release] (#282)

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:16.17
-docker push cimg/node:16.17.0
-docker tag cimg/node:16.17.0 cimg/node:lts
-docker push cimg/node:lts
-docker push cimg/node:16.17-browsers
-docker push cimg/node:16.17.0-browsers
-docker tag cimg/node:16.17.0-browsers cimg/node:lts-browsers
-docker push cimg/node:lts-browsers
+docker push cimg/node:18.8
+docker push cimg/node:18.8.0
+docker tag cimg/node:18.8.0 cimg/node:current
+docker push cimg/node:current
+docker push cimg/node:18.8-browsers
+docker push cimg/node:18.8.0-browsers
+docker tag cimg/node:18.8.0-browsers cimg/node:current-browsers
+docker push cimg/node:current-browsers
