42b40e386a2a0c6f12876e463a5d8f124ca26c49
Jeff Chen <jeff.chen@circleci.com>
2022-09-26 11:34:36 -0700

    Publish v16.17.1, v18.9.1, and v14.20.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 14.20/Dockerfile -t cimg/node:14.20.1 -t cimg/node:14.20 .
-docker build --file 14.20/browsers/Dockerfile -t cimg/node:14.20.1-browsers -t cimg/node:14.20-browsers .
 docker build --file 16.17/Dockerfile -t cimg/node:16.17.1 -t cimg/node:16.17 .
 docker build --file 16.17/browsers/Dockerfile -t cimg/node:16.17.1-browsers -t cimg/node:16.17-browsers .
 docker build --file 18.9/Dockerfile -t cimg/node:18.9.1 -t cimg/node:18.9 .
 docker build --file 18.9/browsers/Dockerfile -t cimg/node:18.9.1-browsers -t cimg/node:18.9-browsers .
+docker build --file 14.20/Dockerfile -t cimg/node:14.20.1 -t cimg/node:14.20 .
+docker build --file 14.20/browsers/Dockerfile -t cimg/node:14.20.1-browsers -t cimg/node:14.20-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:14.20
-docker push cimg/node:14.20.1
-docker push cimg/node:14.20-browsers
-docker push cimg/node:14.20.1-browsers
 docker push cimg/node:16.17
 docker push cimg/node:16.17.1
 docker tag cimg/node:16.17.1 cimg/node:lts
@@ -20,3 +16,7 @@ docker push cimg/node:18.9-browsers
 docker push cimg/node:18.9.1-browsers
 docker tag cimg/node:18.9.1-browsers cimg/node:current-browsers
 docker push cimg/node:current-browsers
+docker push cimg/node:14.20
+docker push cimg/node:14.20.1
+docker push cimg/node:14.20-browsers
+docker push cimg/node:14.20.1-browsers
