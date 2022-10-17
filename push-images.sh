23abfa290968e444531aaeaaa3b6a92b5685000f
Jalexchen <jeff.chen@circleci.com>
2022-10-17 10:39:58 -0700

    Publish v18.11.0. [release] (#295)

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:16.18
-docker push cimg/node:16.18.0
-docker tag cimg/node:16.18.0 cimg/node:lts
-docker push cimg/node:lts
-docker push cimg/node:16.18-browsers
-docker push cimg/node:16.18.0-browsers
-docker tag cimg/node:16.18.0-browsers cimg/node:lts-browsers
-docker push cimg/node:lts-browsers
+docker push cimg/node:18.11
+docker push cimg/node:18.11.0
+docker tag cimg/node:18.11.0 cimg/node:current
+docker push cimg/node:current
+docker push cimg/node:18.11-browsers
+docker push cimg/node:18.11.0-browsers
+docker tag cimg/node:18.11.0-browsers cimg/node:current-browsers
+docker push cimg/node:current-browsers
