a2f0ac3a2918e99b0d417d1c99484f57fc7f6977
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-25 10:24:41 -0400

    Respin v16.18.0 and v18.11.0. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+docker push cimg/node:16.18
+docker push cimg/node:16.18.0
+docker push cimg/node:16.18-browsers
+docker push cimg/node:16.18.0-browsers
 docker push cimg/node:18.11
 docker push cimg/node:18.11.0
 docker tag cimg/node:18.11.0 cimg/node:current
