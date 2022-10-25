b803bffe7ab9892b06579bd81e71827c7dd41832
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-25 10:38:05 -0400

    Respin v16.18.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 19.0/Dockerfile -t cimg/node:19.0.0 -t cimg/node:19.0 .
-docker build --file 19.0/browsers/Dockerfile -t cimg/node:19.0.0-browsers -t cimg/node:19.0-browsers .
+docker build --file 16.18/Dockerfile -t cimg/node:16.18.0 -t cimg/node:16.18 .
+docker build --file 16.18/browsers/Dockerfile -t cimg/node:16.18.0-browsers -t cimg/node:16.18-browsers .
