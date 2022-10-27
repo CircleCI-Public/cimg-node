274c7282edd08af75b972485d39f8b5c8c3ff8d5
Ricardo N Feliciano <FelicianoTech@gmail.com>
2022-10-27 03:58:47 -0400

    Respin v16.18.0, 18.11.0, and 19.0.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -3,3 +3,7 @@
 
 docker build --file 16.18/Dockerfile -t cimg/node:16.18.0 -t cimg/node:16.18 .
 docker build --file 16.18/browsers/Dockerfile -t cimg/node:16.18.0-browsers -t cimg/node:16.18-browsers .
+docker build --file 18.11/Dockerfile -t cimg/node:18.11.0 -t cimg/node:18.11 .
+docker build --file 18.11/browsers/Dockerfile -t cimg/node:18.11.0-browsers -t cimg/node:18.11-browsers .
+docker build --file 19.0/Dockerfile -t cimg/node:19.0.0 -t cimg/node:19.0 .
+docker build --file 19.0/browsers/Dockerfile -t cimg/node:19.0.0-browsers -t cimg/node:19.0-browsers .
--- push-images.sh
+++ push-images.sh
@@ -8,3 +8,15 @@ docker push cimg/node:16.18-browsers
 docker push cimg/node:16.18.0-browsers
 docker tag cimg/node:16.18.0-browsers cimg/node:lts-browsers
 docker push cimg/node:lts-browsers
+docker push cimg/node:18.11
+docker push cimg/node:18.11.0
+docker push cimg/node:18.11-browsers
+docker push cimg/node:18.11.0-browsers
+docker push cimg/node:19.0
+docker push cimg/node:19.0.0
+docker tag cimg/node:19.0.0 cimg/node:current
+docker push cimg/node:current
+docker push cimg/node:19.0-browsers
+docker push cimg/node:19.0.0-browsers
+docker tag cimg/node:19.0.0-browsers cimg/node:current-browsers
+docker push cimg/node:current-browsers
