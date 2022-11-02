2e0be32011a4525492ce15f83b09a441bed0969f
Jeff Chen <jeff.chen@circleci.com>
2022-11-01 18:28:51 -0700

    respin 18.12.0 [release]

--- push-images.sh
+++ push-images.sh
@@ -2,9 +2,5 @@
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/node:18.12
 docker push cimg/node:18.12.0
-docker tag cimg/node:18.12.0 cimg/node:lts
-docker push cimg/node:lts
 docker push cimg/node:18.12-browsers
 docker push cimg/node:18.12.0-browsers
-docker tag cimg/node:18.12.0-browsers cimg/node:lts-browsers
-docker push cimg/node:lts-browsers
