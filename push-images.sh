54d8daa469321c365a2569dfb3e96f5ea4c368f9
Jeff Chen <jeff.chen@circleci.com>
2022-12-14 10:10:38 -0800

    Publish v19.3.0. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:19.2
-docker push cimg/node:19.2.0
-docker tag cimg/node:19.2.0 cimg/node:current
+docker push cimg/node:19.3
+docker push cimg/node:19.3.0
+docker tag cimg/node:19.3.0 cimg/node:current
 docker push cimg/node:current
-docker push cimg/node:19.2-browsers
-docker push cimg/node:19.2.0-browsers
-docker tag cimg/node:19.2.0-browsers cimg/node:current-browsers
+docker push cimg/node:19.3-browsers
+docker push cimg/node:19.3.0-browsers
+docker tag cimg/node:19.3.0-browsers cimg/node:current-browsers
 docker push cimg/node:current-browsers
