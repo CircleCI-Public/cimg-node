1633c6b470b9a781a214e2d6bf252ef3ac5757f2
Adam Hartley <BytesGuy@users.noreply.github.com>
2022-12-29 14:54:08 +0000

    Publish v16.19.0. [release] (#312)
    Re-spin of #311 due to signed commits requirement

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 19.3/Dockerfile -t cimg/node:19.3.0 -t cimg/node:19.3 .
-docker build --file 19.3/browsers/Dockerfile -t cimg/node:19.3.0-browsers -t cimg/node:19.3-browsers .
+docker build --file 16.19/Dockerfile -t cimg/node:16.19.0 -t cimg/node:16.19 .
+docker build --file 16.19/browsers/Dockerfile -t cimg/node:16.19.0-browsers -t cimg/node:16.19-browsers .
