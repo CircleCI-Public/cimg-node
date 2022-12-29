fb237e8258b4bd7d1cb72cbb7c0d540d96dcbe48
Adam Hartley <git@ahartley.com>
2022-12-29 14:51:54 +0000

    Publish v16.19.0. [release]

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:19.3
-docker push cimg/node:19.3.0
-docker tag cimg/node:19.3.0 cimg/node:current
-docker push cimg/node:current
-docker push cimg/node:19.3-browsers
-docker push cimg/node:19.3.0-browsers
-docker tag cimg/node:19.3.0-browsers cimg/node:current-browsers
-docker push cimg/node:current-browsers
+docker push cimg/node:16.19
+docker push cimg/node:16.19.0
+docker push cimg/node:16.19-browsers
+docker push cimg/node:16.19.0-browsers
