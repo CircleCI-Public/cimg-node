923bde6502a20edc29dd8f24ddc6e65708a599d0
Stuart Chinery <163900+schinery@users.noreply.github.com>
2023-01-14 11:02:50 +0000

    Publish v14.21.2, v18.13.0, and v19.4.0. [release] (#313)
    Generated via `./shared/release.sh 14.21.2 18.13.0=lts 19.4.0=current`.

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 16.19/Dockerfile -t cimg/node:16.19.0 -t cimg/node:16.19 .
-docker build --file 16.19/browsers/Dockerfile -t cimg/node:16.19.0-browsers -t cimg/node:16.19-browsers .
+docker build --file 14.21/Dockerfile -t cimg/node:14.21.2 -t cimg/node:14.21 .
+docker build --file 14.21/browsers/Dockerfile -t cimg/node:14.21.2-browsers -t cimg/node:14.21-browsers .
+docker build --file 18.13/Dockerfile -t cimg/node:18.13.0 -t cimg/node:18.13 .
+docker build --file 18.13/browsers/Dockerfile -t cimg/node:18.13.0-browsers -t cimg/node:18.13-browsers .
+docker build --file 19.4/Dockerfile -t cimg/node:19.4.0 -t cimg/node:19.4 .
+docker build --file 19.4/browsers/Dockerfile -t cimg/node:19.4.0-browsers -t cimg/node:19.4-browsers .
