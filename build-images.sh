6724c32fdd8baa9fe0108dbaff6b192a1393035f
Jeff Chen <jeff.chen@circleci.com>
2023-02-06 13:02:53 -0800

    Publish v18.14.0 and v19.6.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 19.5/Dockerfile -t cimg/node:19.5.0 -t cimg/node:19.5 .
-docker build --file 19.5/browsers/Dockerfile -t cimg/node:19.5.0-browsers -t cimg/node:19.5-browsers .
+docker build --file 18.14/Dockerfile -t cimg/node:18.14.0 -t cimg/node:18.14 .
+docker build --file 18.14/browsers/Dockerfile -t cimg/node:18.14.0-browsers -t cimg/node:18.14-browsers .
+docker build --file 19.6/Dockerfile -t cimg/node:19.6.0 -t cimg/node:19.6 .
+docker build --file 19.6/browsers/Dockerfile -t cimg/node:19.6.0-browsers -t cimg/node:19.6-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:19.5
-docker push cimg/node:19.5.0
-docker tag cimg/node:19.5.0 cimg/node:current
+docker push cimg/node:18.14
+docker push cimg/node:18.14.0
+docker tag cimg/node:18.14.0 cimg/node:lts
+docker push cimg/node:lts
+docker push cimg/node:18.14-browsers
+docker push cimg/node:18.14.0-browsers
+docker tag cimg/node:18.14.0-browsers cimg/node:lts-browsers
+docker push cimg/node:lts-browsers
+docker push cimg/node:19.6
+docker push cimg/node:19.6.0
+docker tag cimg/node:19.6.0 cimg/node:current
 docker push cimg/node:current
-docker push cimg/node:19.5-browsers
-docker push cimg/node:19.5.0-browsers
-docker tag cimg/node:19.5.0-browsers cimg/node:current-browsers
+docker push cimg/node:19.6-browsers
+docker push cimg/node:19.6.0-browsers
+docker tag cimg/node:19.6.0-browsers cimg/node:current-browsers
 docker push cimg/node:current-browsers
