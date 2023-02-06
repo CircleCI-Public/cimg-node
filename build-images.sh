b6f449cb64e435d22f2b73c3199d38a9db572e27
Sébastien David (seb) <50570955+alk-sdavid@users.noreply.github.com>
2023-02-06 19:09:25 +0100

    Publish v18.14.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 19.5/Dockerfile -t cimg/node:19.5.0 -t cimg/node:19.5 .
-docker build --file 19.5/browsers/Dockerfile -t cimg/node:19.5.0-browsers -t cimg/node:19.5-browsers .
+docker build --file 18.14/Dockerfile -t cimg/node:18.14.0 -t cimg/node:18.14 .
+docker build --file 18.14/browsers/Dockerfile -t cimg/node:18.14.0-browsers -t cimg/node:18.14-browsers .
