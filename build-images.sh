da6680a952392f218d673738f5f0faac4411bf51
Gary Hawes <gary.hawes@gmail.com>
2023-02-22 15:36:34 +0000

    18.14.2 [release]

--- build-images.sh
+++ build-images.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 14.21/Dockerfile -t cimg/node:14.21.3 -t cimg/node:14.21 .
-docker build --file 14.21/browsers/Dockerfile -t cimg/node:14.21.3-browsers -t cimg/node:14.21-browsers .
-docker build --file 16.19/Dockerfile -t cimg/node:16.19.1 -t cimg/node:16.19 .
-docker build --file 16.19/browsers/Dockerfile -t cimg/node:16.19.1-browsers -t cimg/node:16.19-browsers .
-docker build --file 18.14/Dockerfile -t cimg/node:18.14.1 -t cimg/node:18.14 .
-docker build --file 18.14/browsers/Dockerfile -t cimg/node:18.14.1-browsers -t cimg/node:18.14-browsers .
-docker build --file 19.6/Dockerfile -t cimg/node:19.6.1 -t cimg/node:19.6 .
-docker build --file 19.6/browsers/Dockerfile -t cimg/node:19.6.1-browsers -t cimg/node:19.6-browsers .
+docker build --file 18.14/Dockerfile -t cimg/node:18.14.2 -t cimg/node:18.14 .
+docker build --file 18.14/browsers/Dockerfile -t cimg/node:18.14.2-browsers -t cimg/node:18.14-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,26 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:14.21
-docker push cimg/node:14.21.3
-docker push cimg/node:14.21-browsers
-docker push cimg/node:14.21.3-browsers
-docker push cimg/node:16.19
-docker push cimg/node:16.19.1
-docker push cimg/node:16.19-browsers
-docker push cimg/node:16.19.1-browsers
 docker push cimg/node:18.14
-docker push cimg/node:18.14.1
-docker tag cimg/node:18.14.1 cimg/node:lts
+docker push cimg/node:18.14.2
+docker tag cimg/node:18.14.2 cimg/node:lts
 docker push cimg/node:lts
 docker push cimg/node:18.14-browsers
-docker push cimg/node:18.14.1-browsers
-docker tag cimg/node:18.14.1-browsers cimg/node:lts-browsers
+docker push cimg/node:18.14.2-browsers
+docker tag cimg/node:18.14.2-browsers cimg/node:lts-browsers
 docker push cimg/node:lts-browsers
-docker push cimg/node:19.6
-docker push cimg/node:19.6.1
-docker tag cimg/node:19.6.1 cimg/node:current
-docker push cimg/node:current
-docker push cimg/node:19.6-browsers
-docker push cimg/node:19.6.1-browsers
-docker tag cimg/node:19.6.1-browsers cimg/node:current-browsers
-docker push cimg/node:current-browsers
