cfc79c83790ca79f577677b49786a02604d5e364
Jalexchen <jeff.chen@circleci.com>
2023-02-22 21:06:00 -0800

    Publish v19.7.0. [release] (#323)

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 18.14/Dockerfile -t cimg/node:18.14.2 -t cimg/node:18.14 .
-docker build --file 18.14/browsers/Dockerfile -t cimg/node:18.14.2-browsers -t cimg/node:18.14-browsers .
+docker build --file 19.7/Dockerfile -t cimg/node:19.7.0 -t cimg/node:19.7 .
+docker build --file 19.7/browsers/Dockerfile -t cimg/node:19.7.0-browsers -t cimg/node:19.7-browsers .
