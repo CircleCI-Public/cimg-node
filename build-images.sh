f6deda58e1c231bdee85f815509a4e326e7a4333
Jeff Chen <jeff.chen@circleci.com>
2023-03-17 15:26:18 -0700

    Publish v19.8.0. [release]

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 18.15/Dockerfile -t cimg/node:18.15.0 -t cimg/node:18.15 --platform linux/amd64 .
-docker build --file 18.15/browsers/Dockerfile -t cimg/node:18.15.0-browsers -t cimg/node:18.15-browsers --platform linux/amd64 .
+docker build --file 19.8/Dockerfile -t cimg/node:19.8.0 -t cimg/node:19.8 --platform linux/amd64 .
+docker build --file 19.8/browsers/Dockerfile -t cimg/node:19.8.0-browsers -t cimg/node:19.8-browsers --platform linux/amd64 .
