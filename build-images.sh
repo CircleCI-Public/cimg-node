3d5ac56a826766c72d4022c4f8f63f0831e49c3e
Jalexchen <jeff.chen@circleci.com>
2023-03-22 10:19:32 -0700

    Publish v19.8.1. [release] (#329)
    Co-authored-by: cpe-image-bot <secops+cpe-image-bot@circleci.com>

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 19.8/Dockerfile -t cimg/node:19.8.0 -t cimg/node:19.8 --platform linux/amd64 .
-docker build --file 19.8/browsers/Dockerfile -t cimg/node:19.8.0-browsers -t cimg/node:19.8-browsers --platform linux/amd64 .
+docker build --file 19.8/Dockerfile -t cimg/node:19.8.1 -t cimg/node:19.8 --platform linux/amd64 .
+docker build --file 19.8/browsers/Dockerfile -t cimg/node:19.8.1-browsers -t cimg/node:19.8-browsers --platform linux/amd64 .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/node:19.8
-docker push cimg/node:19.8.0
-docker tag cimg/node:19.8.0 cimg/node:current
+docker push cimg/node:19.8.1
+docker tag cimg/node:19.8.1 cimg/node:current
 docker push cimg/node:current
 docker push cimg/node:19.8-browsers
-docker push cimg/node:19.8.0-browsers
-docker tag cimg/node:19.8.0-browsers cimg/node:current-browsers
+docker push cimg/node:19.8.1-browsers
+docker tag cimg/node:19.8.1-browsers cimg/node:current-browsers
 docker push cimg/node:current-browsers
