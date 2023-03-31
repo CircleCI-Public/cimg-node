71a1bd9c50f186631fd439af958ef31dc7d93264
Jalexchen <jeff.chen@circleci.com>
2023-03-31 11:26:57 -0700

    Publish v16.20.0. [release] (#330)
    Co-authored-by: cpe-image-bot <secops+cpe-image-bot@circleci.com>

--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:19.8
-docker push cimg/node:19.8.1
-docker tag cimg/node:19.8.1 cimg/node:current
-docker push cimg/node:current
-docker push cimg/node:19.8-browsers
-docker push cimg/node:19.8.1-browsers
-docker tag cimg/node:19.8.1-browsers cimg/node:current-browsers
-docker push cimg/node:current-browsers
+docker push cimg/node:16.20
+docker push cimg/node:16.20.0
+docker push cimg/node:16.20-browsers
+docker push cimg/node:16.20.0-browsers
