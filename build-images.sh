6ed9b41a914c4b540317f93e1d1556ff34ab1305
Adam Hartley <git@ahartley.com>
2023-07-05 09:20:30 +0100

    Test build

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 
-docker build --file 20.3/Dockerfile -t cimg/node:20.3.0 -t cimg/node:20.3 --platform linux/amd64 .
-docker build --file 20.3/browsers/Dockerfile -t cimg/node:20.3.0-browsers -t cimg/node:20.3-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 20.3/Dockerfile -t cimg/node:20.3.1 -t cimg/node:20.3 .
+docker buildx build --platform=linux/amd64 --file 20.3/browsers/Dockerfile -t cimg/node:20.3.1-browsers -t cimg/node:20.3-browsers .
--- push-images.sh
+++ push-images.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 docker push cimg/node:20.3
-docker push cimg/node:20.3.0
-docker tag cimg/node:20.3.0 cimg/node:current
+docker push cimg/node:20.3.1
+docker tag cimg/node:20.3.1 cimg/node:current
 docker push cimg/node:current
 docker push cimg/node:20.3-browsers
-docker push cimg/node:20.3.0-browsers
-docker tag cimg/node:20.3.0-browsers cimg/node:current-browsers
+docker push cimg/node:20.3.1-browsers
+docker tag cimg/node:20.3.1-browsers cimg/node:current-browsers
 docker push cimg/node:current-browsers
