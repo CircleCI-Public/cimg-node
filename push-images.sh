99479f18fe61e3f4d9e5f5e4ce6160f360efb0ca
Adam Hartley <git@ahartley.com>
2023-07-06 20:17:02 +0100

    Test without sudo

--- push-images.sh
+++ push-images.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
-docker push cimg/node:20.3
-docker push cimg/node:20.3.1
-docker tag cimg/node:20.3.1 cimg/node:current
+docker push cimg/node:20.4
+docker push cimg/node:20.4.0
+docker tag cimg/node:20.4.0 cimg/node:current
 docker push cimg/node:current
-docker push cimg/node:20.3-browsers
-docker push cimg/node:20.3.1-browsers
-docker tag cimg/node:20.3.1-browsers cimg/node:current-browsers
+docker push cimg/node:20.4-browsers
+docker push cimg/node:20.4.0-browsers
+docker tag cimg/node:20.4.0-browsers cimg/node:current-browsers
 docker push cimg/node:current-browsers
 docker push cimg/node:18.16
 docker push cimg/node:18.16.1
