59c6e749b89457bac16f71c21f5fefcc6ea03f19
Adam Hartley <git@ahartley.com>
2023-07-17 14:58:22 +0100

    test build

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
+set -eo pipefail
 
-docker build --file 20.4/Dockerfile -t cimg/node:20.4.0 -t cimg/node:20.4 --platform linux/amd64 .
-docker build --file 20.4/browsers/Dockerfile -t cimg/node:20.4.0-browsers -t cimg/node:20.4-browsers --platform linux/amd64 .
+docker context create cimg
+docker buildx create --use cimg
+docker buildx build --platform=linux/amd64,linux/arm64 --file 20.4/Dockerfile -t cimg/node:20.4.0 -t cimg/node:20.4 --push .
+docker buildx build --platform=linux/amd64 --file 20.4/browsers/Dockerfile -t cimg/node:20.4.0-browsers -t cimg/node:20.4-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 18.16/Dockerfile -t cimg/node:18.16.1 -t cimg/node:18.16 --push .
+docker buildx build --platform=linux/amd64 --file 18.16/browsers/Dockerfile -t cimg/node:18.16.1-browsers -t cimg/node:18.16-browsers --push .
