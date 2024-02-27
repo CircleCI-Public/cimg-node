cf5907433119c488b8d6c3f2abd111cd9e2a67a9
Stuart Chinery <stuart.chinery@boxt.co.uk>
2024-02-27 14:19:02 +0000

    Publish v21.6.2, v20.11.1, and v18.19.1. [release]

--- build-images.sh
+++ build-images.sh
@@ -4,5 +4,9 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 21.6/Dockerfile -t cimg/node:21.6.1 -t cimg/node:21.6 --push .
-docker buildx build --platform=linux/amd64 --file 21.6/browsers/Dockerfile -t cimg/node:21.6.1-browsers -t cimg/node:21.6-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 21.6/Dockerfile -t cimg/node:21.6.2 -t cimg/node:21.6 --push .
+docker buildx build --platform=linux/amd64 --file 21.6/browsers/Dockerfile -t cimg/node:21.6.2-browsers -t cimg/node:21.6-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 20.11/Dockerfile -t cimg/node:20.11.1 -t cimg/node:20.11 --push .
+docker buildx build --platform=linux/amd64 --file 20.11/browsers/Dockerfile -t cimg/node:20.11.1-browsers -t cimg/node:20.11-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 18.19/Dockerfile -t cimg/node:18.19.1 -t cimg/node:18.19 --push .
+docker buildx build --platform=linux/amd64 --file 18.19/browsers/Dockerfile -t cimg/node:18.19.1-browsers -t cimg/node:18.19-browsers --push .
