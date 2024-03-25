850d99d1fcce485c063b50d9e3191123ea05084f
Olivier Tremblay <olivier@circleci.com>
2024-03-25 08:51:47 -0400

    Sort 21.7 versions chronologically

--- build-images.sh
+++ build-images.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 docker context create cimg
 docker buildx create --use cimg
-docker buildx build --platform=linux/amd64,linux/arm64 --file 21.7/Dockerfile -t cimg/node:21.7.1 -t cimg/node:21.7 --push .
-docker buildx build --platform=linux/amd64 --file 21.7/browsers/Dockerfile -t cimg/node:21.7.1-browsers -t cimg/node:21.7-browsers --push .
 docker buildx build --platform=linux/amd64,linux/arm64 --file 21.7/Dockerfile -t cimg/node:21.7.0 -t cimg/node:21.7 --push .
 docker buildx build --platform=linux/amd64 --file 21.7/browsers/Dockerfile -t cimg/node:21.7.0-browsers -t cimg/node:21.7-browsers --push .
+docker buildx build --platform=linux/amd64,linux/arm64 --file 21.7/Dockerfile -t cimg/node:21.7.1 -t cimg/node:21.7 --push .
+docker buildx build --platform=linux/amd64 --file 21.7/browsers/Dockerfile -t cimg/node:21.7.1-browsers -t cimg/node:21.7-browsers --push .
--- push-images.sh
+++ push-images.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Do not edit by hand; please use build scripts/templates to make changes
 set -eo pipefail
-docker buildx imagetools create -t cimg/node:current cimg/node:21.7.1
-docker buildx imagetools create -t cimg/node:current-browsers cimg/node:21.7.1-browsers
 docker buildx imagetools create -t cimg/node:current cimg/node:21.7.0
 docker buildx imagetools create -t cimg/node:current-browsers cimg/node:21.7.0-browsers
+docker buildx imagetools create -t cimg/node:current cimg/node:21.7.1
+docker buildx imagetools create -t cimg/node:current-browsers cimg/node:21.7.1-browsers
